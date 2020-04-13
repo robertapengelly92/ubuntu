45d9bfc08fcaddbbd3259da1508aefd4255fb987
Robert Pengelly <robertapengelly@hotmail.co.uk>
2020-04-13 13:45:17 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+sudo su
+sync; echo 3 > /proc/sys/drop_caches
+
+echo vm.swappiness=10 > /etc/sysctl.conf
+sysctl -p
+
+echo "unset HISTFILE" > /etc/bash.bashrc
+
+echo QT_QPA_PLATFORMTHEME=gtk2 > /etc/environment
+echo QT_STYLE_OVERRIDE > /etc/environment
+
+OLD=GRUB_TIMEOUT=10
+OUTPUT=""
+NEW=GRUB_TIMEOUT=3
+while read LINE || [ "$LINE" ]; do
+	OUTPUT="${OUTPUT}${LINE//$OLD/$NEW}
+done < /etc/default/grub
+printf "${OUTPUT}" > "${FILE]"
+update-grub
+
+apt purge -y gstreamer1.0-fluendo-mp3 snapd whoopsie whoopsie-preferences
+apt install -y gnome-calculator gnome-characters gnome-logs gnome-system-monitor
+apt install -y chrome-gnome-shell gnome-tweaks ubuntu-restricted-extras
+apt install -y binutils bison flex gcc gcc-multilib make nasm nautilus-wip
+
+add-apt-repository -y ppa:fossproject/ppa
+add-adt-repository -y ppa:linuxuprising/java
+add-apt-repository -y ppa:notepadqq-team/notepadqq
+
+apt install -y green-recorder notepadqq oracle-java14-installer
+apt install -y git imagemagick libncursesw5 qt5-style-plugins p7zip-full p7zip-rar pkg-config sqlite3 webp
+apt install -y qemu qemu-block-extra qemu-slof qemu-system qemu-user qemu-utils
+apt install -y cleamentine deluge gdebi-core ghex gimp gnome-control-center isomaster k3b kdenlive libreoffice python-pip python3-pip simplescreenrecorder vlc
+
+adduser $USER kvme
+chmod 4711 /usr/bin/wodim; sudo chmod 4711 /usr/bin/cdrdao
+wget -qO- http://plasmasturm.org/code/vistafonts-installer/vistafonts-installer | bash
+
+exit
+
+echo [QT] > ~/Trolltech.conf
+echo style=GTK+ > /Trolltech.conf
+
+gsettings set org.gnome.shell enable-hot-corners true
+gsettings set org.gnome.shell.extensions.hash-to-dock click-action 'minimize'
+gsettings set com.ubuntu.update-notifier show-livepatch-status-icon false
