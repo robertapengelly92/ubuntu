419cc3abec887a6687965ea9c3077dca928ae178
Robert Pengelly <robertapengelly@hotmail.co.uk>
2020-09-06 11:54:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -37,6 +37,12 @@ apt -y install bless clementine deluge devede gdebi-core ghex gimp gnome-control
 chmod 4711 /usr/bin/wodim
 chmod 4711 /usr/bin/cdrdao
 
+apt -y install apt-transport-https curl
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
+apt update
+apt -y install brave-browser
+
 apt -y purge --autoremove
 exit
 
