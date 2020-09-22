61e25f87a11ec347b31221ad2a3b81bcca533e45
Robert Pengelly <robertapengelly@hotmail.co.uk>
2020-09-22 16:06:18 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,7 +33,8 @@ apt -y install git imagemagick libncursesw5 qt5-style-plugins p7zip-full p7zip-r
 apt -y install qemu qemu-block-extra qemu-kvm qemu-slof qemu-system qemu-user qemu-utils || exit 1
 apt -y install bochs bochsbios bochs-x vgabios || exit 1
 apt -y install bless clementine deluge devede gdebi-core ghex gimp gir1.2-gmenu-3.0 gnome-control-center gnome-menus isomaster k3b kdenlive libreoffice python3 python3-pip simplescreenrecorder usb-creator-gtk vlc || exit 1
-#apy -y install gnome-shell-extensions
+#apt -y install gnome-shell-extensions
+
 chmod 4711 /usr/bin/wodim
 chmod 4711 /usr/bin/cdrdao
 
