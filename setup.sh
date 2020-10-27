631097f2ebfde9ccaf0ae2c0163809de396e51fe
Robert Pengelly <robertapengelly@hotmail.co.uk>
2020-10-27 03:01:51 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -32,8 +32,8 @@ apt -y install oracle-java15-installer oracle-java15-set-default || exit 1
 apt -y install gettext git imagemagick libncursesw5 qt5-style-plugins p7zip-full p7zip-rar pkg-config sqlite3 texinfo webp || exit 1
 apt -y install qemu qemu-block-extra qemu-kvm qemu-slof qemu-system qemu-user qemu-utils || exit 1
 apt -y install bochs bochsbios bochs-x vgabios || exit 1
-apt -y install bless clementine deluge devede gdebi-core ghex gimp gir1.2-gmenu-3.0 gnome-control-center gnome-menus isomaster k3b kdenlive libreoffice python3 python3-pip simplescreenrecorder usb-creator-gtk vlc || exit 1
-#apt -y install gnome-shell-extensions
+apt -y install clementine deluge devede gdebi-core ghex gimp gir1.2-gmenu-3.0 gnome-control-center gnome-menus isomaster k3b kdenlive libreoffice python3 python3-pip simplescreenrecorder usb-creator-gtk vlc || exit 1
+#apt -y install bless gnome-shell-extensions
 
 chmod 4711 /usr/bin/wodim
 chmod 4711 /usr/bin/cdrdao
