d1b0678df2606742795fcf1ccd442d50b9ea03de
Robert Pengelly <robertapengelly@hotmail.co.uk>
2025-07-04 18:21:44 +0100

    Update setup_arm.sh

--- setup_arm.sh
+++ setup_arm.sh
@@ -47,8 +47,10 @@ apt -y install clementine firefox ghex gimp gir1.2-gmenu-3.0 gnome-control-cente
 
 apt -y install apt-transport-https curl || exit 1
 
-#wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/packages.microsoft.gpg
-#sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
 
 apt update
 
